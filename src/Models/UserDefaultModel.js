b3b168620c8dd5c2856087d04cfece8e2ee6934f
matheusmadeira <fumacammadeira@gmail.com>
2025-02-20 19:11:30 -0300

    feat: linking register page

--- src/Models/UserDefaultModel.js
+++ src/Models/UserDefaultModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const UserModelSchema = new Schema({
-  name: {
+  nome: {
     type: String,
     required: true,
     trim: true,
